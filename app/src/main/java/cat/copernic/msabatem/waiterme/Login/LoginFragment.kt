eda7827fd8eb1fafd3342b3ee5ac69301fb21a85
msabate00 <sabatemiranda@gmail.com>
2022-02-21 17:53:07 +0100

    MSM - Register/Login terminados

--- app/src/main/java/cat/copernic/msabatem/waiterme/Login/LoginFragment.kt
+++ app/src/main/java/cat/copernic/msabatem/waiterme/Login/LoginFragment.kt
@@ -1,12 +1,20 @@
 package cat.copernic.msabatem.waiterme.Login
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import cat.copernic.msabatem.waiterme.R
 import cat.copernic.msabatem.waiterme.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 /**
@@ -17,9 +25,11 @@ import cat.copernic.msabatem.waiterme.databinding.FragmentLoginBinding
 class LoginFragment : Fragment() {
 
     private lateinit var binding: FragmentLoginBinding
+    private lateinit var auth: FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
 
     }
 
@@ -30,8 +40,63 @@ class LoginFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentLoginBinding.inflate(inflater, container, false);
 
+        binding.btLoginSingIn.setOnClickListener {
+            signIn();
+        }
+        binding.btLoginSingUp.setOnClickListener {
+            findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRegisterFragment());
+        }
+
         return binding.root;
         //return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        auth.signOut();
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            reload();
+        }
+    }
+
+    private fun signIn() {
+        // [START sign_in_with_email]
+
+
+
+        val email = binding.etLoginEmail.text.toString();
+        val password = binding.etLoginPassword.text.toString();
+
+        if(email != "" && password != ""){
+
+            Log.i("AYUDA", "$email __ $password");
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this.requireActivity()) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = auth.currentUser
+                        updateUI(user)
+                        findNavController().navigate(LoginFragmentDirections.actionLoginFragmentToRolSelectorFragment());
+
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "signInWithEmail:failure", task.exception)
+                        Toast.makeText(context, R.string.auth_failed,
+                            Toast.LENGTH_SHORT).show()
+                        updateUI(null)
+                    }
+                }
+        }
+    }
+
+
+    private fun reload(){
+        updateUI(auth.currentUser)
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+
     }
 
 
--- app/src/main/java/cat/copernic/msabatem/waiterme/Register/RegisterFragment.kt
+++ app/src/main/java/cat/copernic/msabatem/waiterme/Register/RegisterFragment.kt
@@ -1,13 +1,20 @@
 package cat.copernic.msabatem.waiterme.Login
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import cat.copernic.msabatem.waiterme.R
 import cat.copernic.msabatem.waiterme.databinding.FragmentLoginBinding
 import cat.copernic.msabatem.waiterme.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 /**
@@ -18,6 +25,7 @@ import cat.copernic.msabatem.waiterme.databinding.FragmentRegisterBinding
 class RegisterFragment : Fragment() {
 
     private lateinit var binding: FragmentRegisterBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,8 +39,63 @@ class RegisterFragment : Fragment() {
         // Inflate the layout for this fragment
         binding = FragmentRegisterBinding.inflate(inflater, container, false);
 
+        binding.btRegisterGoBack.setOnClickListener {
+            findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToLoginFragment());
+        }
+        binding.btRegisterSingUp.setOnClickListener {
+            if(binding.etRegisterEmail.text.toString() != "" && binding.etRegisterPassword.text.toString() != ""){
+                if(binding.etRegisterConfirmPassword.text.toString() == binding.etRegisterPassword.text.toString()){
+                    if(binding.etRegisterPassword.text.toString().length >=6 ){
+                        createAccount();
+                    }else{
+                        Toast.makeText(context, getString(R.string.pass_too_short), Toast.LENGTH_SHORT).show()
+                    }
+                }else{
+                    Toast.makeText(context, getString(R.string.pass_not_match), Toast.LENGTH_SHORT).show()
+                }
+            }else{
+                Toast.makeText(context,getString(R.string.empty_values), Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+
         return binding.root;
         //return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        auth = Firebase.auth
+    }
+
+
+    private fun createAccount() {
+        // [START create_user_with_email]
+        val email = binding.etRegisterEmail.text.toString();
+        val password = binding.etRegisterPassword.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("AYUDA", "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                    findNavController().navigate(RegisterFragmentDirections.actionRegisterFragmentToRolSelectorFragment())
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("AYUDA", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(context, getString(R.string.already_user),
+                        Toast.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    fun updateUI(user: FirebaseUser?){
+
     }
 
 
