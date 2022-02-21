3515bb985786ab8a232a09282f7311c27d0c7a1b
msabate00 <sabatemiranda@gmail.com>
2022-02-21 18:34:34 +0100

    MSM - Popup superadmin (no se ha probado)

--- app/src/main/java/cat/copernic/msabatem/waiterme/RolSelector/RolSelectorFragment.kt
+++ app/src/main/java/cat/copernic/msabatem/waiterme/RolSelector/RolSelectorFragment.kt
@@ -1,13 +1,14 @@
 package cat.copernic.msabatem.waiterme.Login
 
+import android.app.AlertDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.text.InputFilter
+import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import cat.copernic.msabatem.waiterme.R
-import cat.copernic.msabatem.waiterme.databinding.FragmentLoginBinding
-import cat.copernic.msabatem.waiterme.databinding.FragmentRegisterBinding
+import android.widget.EditText
+import androidx.fragment.app.Fragment
 import cat.copernic.msabatem.waiterme.databinding.FragmentRolSelectorBinding
 
 
@@ -19,6 +20,7 @@ import cat.copernic.msabatem.waiterme.databinding.FragmentRolSelectorBinding
 class RolSelectorFragment : Fragment() {
 
     private lateinit var binding: FragmentRolSelectorBinding
+    private lateinit var builder: AlertDialog.Builder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +33,16 @@ class RolSelectorFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = FragmentRolSelectorBinding.inflate(inflater, container, false);
+
+
+        binding.btRolSelectorAdmin.setOnClickListener {
+            builder = AlertDialog.Builder(context)
+            builder.setTitle("Title")
+            val input = EditText(context);
+            input.inputType = InputType.TYPE_NUMBER_FLAG_SIGNED;
+            input.filters[0] = InputFilter.LengthFilter(4);
+            builder.setView(input);
+        }
 
         return binding.root;
         //return inflater.inflate(R.layout.fragment_login, container, false)
